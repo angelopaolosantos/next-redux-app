bf3490667fad5758c9889af389cb345e0b9f2fb7
Angelo Santos <angelopaolosantos@gmail.com>
2019-10-27 21:21:33 -0700

    Create Coming Soon Page

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,27 @@ import Template from '../components/Template'
 
 const index = () => {
     return (
-        <Template>Hello World!</Template>
+        <div className="container">
+            <img src="img/trovegeek-logo2.png" width="250" />
+            <span className="msg">Soon to Open!</span>
+        <style jsx>{`
+        .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            min-height: 100vh;
+        }
+        .msg {
+            font-size: 36px;
+            font-family: Calibri, Helvetica, sans-serif; 
+            color: gray;
+            font-weight: bold;
+            margin: 15px;
+        }
+        `}
+        </style>
+        </div>
     )
 }
 
